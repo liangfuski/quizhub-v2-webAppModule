ea2b4f8da1c5bf83f2ee09a6594635dd94a362a3
Fu <tony.f.liang@gmail.com>
2024-10-06 23:14:23 -0600

    question form display Data init

--- frontend/src/app/manage-quizes/[_id]/page.tsx
+++ frontend/src/app/manage-quizes/[_id]/page.tsx
@@ -1,12 +1,26 @@
 import { getQuizQuestionsByQuizId } from "@/api/route";
+import { Padding } from "@mui/icons-material";
+import { Box } from "@mui/material";
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 export default async function QuizEditPage({ params} : { params: { _id: string }} ) {
     const quizId = params._id;
     const questionList = await getQuizQuestionsByQuizId({quizId})
-    console.log(questionList)
+    console.log("called quiz::", quizId);
     return (
-        <div/>
-    )
+        <div>
+            {quizId}
+            {questionList.map(ele => (<div style={{padding:"10px"}}>
+                <div>{ele.id}</div>
+                <div>{ele.prompt}</div>
+                <div>{ele.answer}</div>
+                <div>{
+                    ele.options.join("  ") 
+                    
+                    }</div>
+            </div>)
+            )}
+        </div>
+    )   
 }
 /* eslint-disable @typescript-eslint/no-unused-vars */
